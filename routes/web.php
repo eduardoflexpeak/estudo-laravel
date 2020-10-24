72d9d5c7b488891ae7a640637781451d6b667651
Eduardo Silva <eduardo@flexpeak.com.br>
2020-10-24 11:54:19 -0400

    Autenticação

--- routes/web.php
+++ routes/web.php
@@ -27,4 +27,11 @@ Route::get('pessoas/{pessoa}', 'PessoaController@show');
 Route::delete('pessoas/{pessoa}', 'PessoaController@destroy');
 */
 
-Route::resource('pessoas', 'PessoaController');
+Route::middleware('auth')->group(function () {
+    // Aqui dentro ficam as rotas protegidas
+    Route::resource('pessoas', 'PessoaController');
+});
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
