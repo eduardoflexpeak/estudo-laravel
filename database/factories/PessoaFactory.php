f1ef07462522e9000cadf4ca590b245a9d5b0f1f
Eduardo Silva <eduardo@flexpeak.com.br>
2020-10-31 10:37:57 -0400

    Populando o banco com Factory

---
+++ database/factories/PessoaFactory.php
@@ -0,0 +1,15 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Pessoa;
+use Faker\Generator as Faker;
+
+$factory->define(Pessoa::class, function (Faker $faker) {
+    return [
+        'nome' => $faker->name,
+        'telefone' => $faker->tollFreePhoneNumber,
+        'email' => $faker->unique()->safeEmail,
+        'user_id' => $faker->numberBetween(1, 2)
+    ];
+});
