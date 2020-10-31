0c491adcc015bc2f24da7c7c743ece277d7bbd8c
Eduardo Silva <eduardo@flexpeak.com.br>
2020-10-31 10:10:12 -0400

    Populando o banco com Seeder

---
+++ database/seeds/UserSeeder.php
@@ -0,0 +1,35 @@
+<?php
+
+use App\User;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::firstOrCreate(
+            [
+                'email' => 'admin@email.com'
+            ],
+            [
+                'name' => 'Administrador',
+                'password' => bcrypt('admin')
+            ]
+        );
+
+        User::firstOrCreate(
+            [
+                'email' => 'funcionario@email.com'
+            ],
+            [
+                'name' => 'Funcionário',
+                'password' => bcrypt('funcionario')
+            ]
+        );
+    }
+}
